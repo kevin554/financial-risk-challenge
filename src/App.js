d96972331cb370be2895bafac9f00334a1eb28e7
Kevin Nicolas Duran Rocha <kevinduran1997@gmail.com>
2021-06-24 10:46:42 -0400

    Using router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Header from './components/header';
+import Home from './pages/home';
+import Calculator from './pages/calculator';
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>
+    <Router>
+      <div>
+        <Header />
+
+        <Switch>
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/calculator" component={Calculator} />
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </div>
+    </Router>
+  </div>;
 }
 
 export default App;
