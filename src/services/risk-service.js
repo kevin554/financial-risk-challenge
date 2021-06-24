f59a4455146d7be3bedbafa2dccb39c69dd26d10
Kevin Nicolas Duran Rocha <kevinduran1997@gmail.com>
2021-06-24 10:40:30 -0400

    Create risk-service.js

---
+++ src/services/risk-service.js
@@ -0,0 +1,95 @@
+export const HEADER = [
+    {
+      key: "bond",
+      value: "Bonds %",
+    },
+    {
+      key: "large",
+      value: "Large cap %",
+    }, 
+    {
+      key: "mid",
+      value: "Mid cap %",
+    }, 
+    {
+      key: "foreign",
+      value: "Foreign %",
+    }, 
+    {
+      key: "small",
+      value: "Small cap %",
+    }, 
+];
+
+export const riskFormulas = [
+    {
+        bond: 80, 
+        large: 20,
+        mid: 0,
+        foreign: 0,
+        small: 0,
+    },
+    {
+        bond: 70, 
+        large: 15,
+        mid: 15,
+        foreign: 0,
+        small: 0,
+    },
+    {
+        bond: 60, 
+        large: 15,
+        mid: 15,
+        foreign: 10,
+        small: 0,
+    },
+    {
+        bond: 50, 
+        large: 20,
+        mid: 20,
+        foreign: 10,
+        small: 0,
+    },
+    {
+        bond: 40, 
+        large: 20,
+        mid: 20,
+        foreign: 20,
+        small: 0,
+    },
+    {
+        bond: 35, 
+        large: 25,
+        mid: 5,
+        foreign: 30,
+        small: 5,
+    },
+    {
+        bond: 20, 
+        large: 25,
+        mid: 25,
+        foreign: 25,
+        small: 5,
+    },
+    {
+        bond: 10, 
+        large: 20,
+        mid: 40,
+        foreign: 20,
+        small: 10,
+    },
+    {
+        bond: 5, 
+        large: 15,
+        mid: 40,
+        foreign: 25,
+        small: 15,
+    },
+    {
+        bond: 0, 
+        large: 5,
+        mid: 25,
+        foreign: 30,
+        small: 40,
+    },
+];
